aa249564a794edf5d4d949993a33d5b4dafa70c5
Lance0567 <esurenalance08@gmail.com>
2025-11-03 14:38:53 +0800

    hotfix: Error fixed on taking a photo again by stuping and closing picamera2

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ BLYNK_AUTH = "wZ5IP73LpgMdLK1PDRnGEFBLHzDagQZq"
 blynk = Blynk(BLYNK_AUTH)
 
 camera = None
+streaming_active = False  # This should track V1 state changes
 
 @blynk.on("connected")
 def blynk_connected(*args, **kwargs):   # Accepts any arguments
@@ -17,30 +18,41 @@ def blynk_connected(*args, **kwargs):   # Accepts any arguments
 
 @blynk.on("V0")
 def on_v0(value):
-    global camera
+    global camera, streaming_active
     val = int(value[0])
     if val == 1:
+        if streaming_active:
+            stop_hls_stream()
+            stop_flask_server()
+            streaming_active = False
+            blynk.virtual_write(1, 0)
         from picamera2 import Picamera2
         camera = Picamera2()
         camera.configure(camera.create_preview_configuration(main={"size": (840, 560), "format": "RGB888"}))
         camera.start()
         handle_take_photo(blynk, camera)
+        camera.stop()
+        camera.close()
+        camera = None
     else:
         if camera:
             camera.stop()
+            camera.close()
             camera = None
-            print("Picamera2 stopped and released.")
 
 @blynk.on("V1")
 def on_v1(value, *args, **kwargs):
+    global streaming_active
     val = int(value[0])
     if val == 1:
         start_flask_server()
         start_hls_stream()
+        streaming_active = True
         blynk.set_property(1, "url", "https://pi.ustfloodcontrol.site/video/index.m3u8")
     else:
         stop_hls_stream()
         stop_flask_server()
+        streaming_active = False
 
 try:
     while True:
